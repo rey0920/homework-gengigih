b2ed7fa95b9b1b99efa7da43d40242f6c5159fbe
rey0920 <avikih0920@gmail.com>
2022-03-23 16:27:37 +0700

    create component album

---
+++ src/pages/playlist/Index.js
@@ -0,0 +1,17 @@
+const Playlist = () => {
+  return (
+    <div className="playlist">
+      <div className="album">
+        <img
+          src="https://i.scdn.co/image/ab67616d00001e02e8b066f70c206551210d902b"
+          alt="gambar"
+        />
+        <p>Bohemian Rhapsody (The Original Soundtrack)</p>
+        <p>Queen</p>
+        <button>Select</button>
+      </div>
+    </div>
+  );
+};
+
+export default Playlist;
